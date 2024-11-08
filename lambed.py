728c8be6a649a7a10ac8a51c9db5dea958420a5f
big boris <supitirin3l@gmail.com>
2024-11-08 22:39:01 +0200

    tegin koik kysimused ara vst, vahest ei taha kinni minna, tean et peaksin eraldi branchi tegema aga mai joua

--- lambed.py
+++ lambed.py
@@ -15,17 +15,34 @@
 ##################################################
 
 from easygui import *
+jatkub=True
+while jatkub:
+    koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel", "Kia"]
+    klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
 
-koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel"]
-klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
+    nimi = enterbox("Mis on su nimi?","Tere!","",True)
+    kindel_ei = multchoicebox("On sul juba teada, mida kindlasti EI soovi?","Kindlad EI-d",koik_margid,None)
 
-nimi = enterbox("Mis on su nimi?","Tere!","",True)
-kindel_ei = multchoicebox("On sul juba teada, mida kindlasti EI soovi?","Kindlad EI-d",koik_margid,None)
+    #tegeleme edaspidi vaid nende autodega, millest kasutaja huvitatud on
+    if kindel_ei != None:
+        for mark in kindel_ei:
+            koik_margid.remove(mark)
+    if len(koik_margid)== 0:
+        jatkub=False
 
-#tegeleme edaspidi vaid nende autodega, millest kasutaja huvitatud on
-if kindel_ei != None:
-    for mark in kindel_ei:
-        koik_margid.remove(mark)
+    #nendele, mis kasutajale meeldivad, lisame hiljem kordaja?
+    pigem_jah = multchoicebox("Äkki on ka teada, milliseid marke eelistad?","Pigem need",koik_margid)
 
-#nendele, mis kasutajale meeldivad, lisame hiljem kordaja?
-pigem_jah = multchoicebox("Äkki on ka teada, milliseid marke eelistad?","Pigem need",koik_margid)
+    budgetnupud=["Minimaalne", "Keskmine", "Okei", "Suur", "Lõpmatu"]
+    budget=buttonbox("Mis on su eelarve?", choices=budgetnupud)
+    nitroseksuaal=buttonbox("Kas oled nitroseksuaal?", choices=["Jah", "Ei"])
+    if nitroseksuaal=="Jah":
+        missugunens=buttonbox("Missugune täpsemalt?", choices=["Klassika", "Drift", "Racing"])
+    remondivalmis=buttonbox("Kas oled valmis remondiks?", choices=["Jah", "Ei"])
+    if remondivalmis=="Ei":
+        jatkub=False
+    kasise=buttonbox("Kas kavatsed ise teha?", choices=["Jah","Ei"])
+    jatkub=False
+tekst="Peaksid vaatama järgnevaud autosid: "
+soovitused="Meie isiklikud soovitused on.."
+popup=msgbox(tekst)
