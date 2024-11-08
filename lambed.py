48babc8b1b9d5fcb68658b3d8b64f4e62292c2ad
Kriset <kriset@laiuste.ee>
2024-11-08 19:28:05 +0200

    easygui ja algne kommentaar

--- lambed.py
+++ lambed.py
@@ -1,1 +1,24 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Lämbed tsõõrid
+# Teema: Kasutajale meelepärase auto soovitamine
+#
+#
+# Autorid: Kriset Laiuste, Pilleriin Sillakivi
+#
+# mõningane eeskuju: isiklik kogemus, online-küsitlused 
+#
+# Lisakommentaar: pip install easygui
+#
+##################################################
+
+from easygui import *
+
 klassika=["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
+
+def main():
+    msgbox("mjau")
+
+main()
