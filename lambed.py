dda8926d4da13d009250be9dfcab317a1f25e5d0
Kriset <kriset@laiuste.ee>
2024-11-08 19:47:51 +0200

    algne eelistusevalik

--- lambed.py
+++ lambed.py
@@ -10,15 +10,22 @@
 #
 # mõningane eeskuju: isiklik kogemus, online-küsitlused 
 #
-# Lisakommentaar: pip install easygui
+# Lisakommentaar (nt kasutusjuhend): 
 #
 ##################################################
 
 from easygui import *
 
-klassika=["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
+koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel"]
+klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
 
-def main():
-    msgbox("mjau")
+nimi = enterbox("Mis on su nimi?","Tere!","",True)
+kindel_ei = multchoicebox("On sul juba teada, mida kindlasti EI soovi?","Kindlad EI-d",koik_margid,None)
 
-main()
+#tegeleme edaspidi vaid nende autodega, millest kasutaja huvitatud on
+if kindel_ei != None:
+    for mark in kindel_ei:
+        koik_margid.remove(mark)
+
+#nendele, mis kasutajale meeldivad, lisame hiljem kordaja?
+pigem_jah = multchoicebox("Äkki on ka teada, milliseid marke eelistad?","Pigem need",koik_margid)
