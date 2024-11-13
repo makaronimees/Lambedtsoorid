f0a5547d29c2354a81b0b894de4bf365468e89c6
Kriset <kriset@laiuste.ee>
2024-11-13 20:49:36 +0200

    kommentaar ja viimistlus esitamiseks

--- lambed.py
+++ lambed.py
@@ -1,4 +1,4 @@
-################################################
+###############################################################################
 # Programmeerimine I
 # 2024/2025 sügissemester
 #
@@ -12,15 +12,31 @@
 #
 # Lisakommentaar (nt kasutusjuhend): 
 #
-##################################################
+###############################################################################
+#
+# Kuidas senine koostöö sujunud? 
+#   Probleeme ei ole tekkinud, mõlemad oleme piisavalt panustanud.
+# Kuidas on rollid jagatud? 
+#   Hetkel proovime koos algvundamendi kokku panna.
+# Kui palju aega projektile on kulunud? 
+#   Kokku umbes 10h koos planeerimisega.
+# Millised on projektiga seoses edasised plaanid ja edasiarendused? 
+#   Lisame funktsionaalsuse, mis oskab tegelikult midagi soovitada.
+#   Selleks peame ilmselt lisama funktsioone ning rohkem andmeid ja kordajaid.
+#   Lisaks võiks struktuuri parandada - praegu on rõhk sellel, et asi toimiks.
+#
+###############################################################################
 
 from easygui import *
+
+
 jatkub=True
 while jatkub:
     koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel", "Kia"]
     klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
 
     nimi = enterbox("Mis on su nimi?","Tere!","",True)
+
     kindel_ei = multchoicebox("On sul juba teada, mida kindlasti EI soovi?","Kindlad EI-d",koik_margid,None)
 
     #tegeleme edaspidi vaid nende autodega, millest kasutaja huvitatud on
@@ -28,21 +44,26 @@ while jatkub:
         for mark in kindel_ei:
             koik_margid.remove(mark)
     if len(koik_margid)== 0:
+        #hiljem kõik jatkub false asendada mingi head aega sõnumiga?
         jatkub=False
 
-    #nendele, mis kasutajale meeldivad, lisame hiljem kordaja?
+    #nendele, mis kasutajale meeldivad, lisame hiljem kordaja? et suurema tõenäosusega neid soovitada
     pigem_jah = multchoicebox("Äkki on ka teada, milliseid marke eelistad?","Pigem need",koik_margid)
 
     budgetnupud=["Minimaalne", "Keskmine", "Okei", "Suur", "Lõpmatu"]
     budget=buttonbox("Mis on su eelarve?", choices=budgetnupud)
+
     nitroseksuaal=buttonbox("Kas oled nitroseksuaal?", choices=["Jah", "Ei"])
     if nitroseksuaal=="Jah":
         missugunens=buttonbox("Missugune täpsemalt?", choices=["Klassika", "Drift", "Racing"])
+    
     remondivalmis=buttonbox("Kas oled valmis remondiks?", choices=["Jah", "Ei"])
     if remondivalmis=="Ei":
         jatkub=False
     kasise=buttonbox("Kas kavatsed ise teha?", choices=["Jah","Ei"])
     jatkub=False
+
+
 tekst="Peaksid vaatama järgnevaud autosid: "
 soovitused="Meie isiklikud soovitused on.."
 popup=msgbox(tekst)
