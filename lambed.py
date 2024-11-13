dc77f43af7a999a863c7e6e1c36d3aac8e43ba34
big boris <supitirin3l@gmail.com>
2024-11-13 21:43:05 +0200

    panin paar ifi ns valikute juurde, sealt saab parast edasi arendada. tegin ka paar uut nimekirja markidega

--- lambed.py
+++ lambed.py
@@ -24,7 +24,7 @@
 #   Lisame funktsionaalsuse, mis oskab tegelikult midagi soovitada.
 #   Selleks peame ilmselt lisama funktsioone ning rohkem andmeid ja kordajaid.
 #   Lisaks võiks struktuuri parandada - praegu on rõhk sellel, et asi toimiks.
-#
+#   Teeme ilmselt funktsiooni, mis arvutab meie loodud kordaja, mille põhjal kasutajale sobivad autod soovitatakse
 ###############################################################################
 
 from easygui import *
@@ -34,6 +34,9 @@ jatkub=True
 while jatkub:
     koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel", "Kia"]
     klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
+    driftikad=["BMW", "Nissan", "Mazda", "Opel Omega"]
+    kiiredpillid=["Subaru", "BMW", "Ford", "Audi", "Mercedes-Benz"]
+    tsipalux=["Porsche", "veel midagi"]
 
     nimi = enterbox("Mis on su nimi?","Tere!","",True)
 
@@ -52,10 +55,18 @@ while jatkub:
 
     budgetnupud=["Minimaalne", "Keskmine", "Okei", "Suur", "Lõpmatu"]
     budget=buttonbox("Mis on su eelarve?", choices=budgetnupud)
-
+    if budget == "Suur":
+        valikudlisa=tsipalux
+    #huvitav kas see on nilbe sõnapruuk?
     nitroseksuaal=buttonbox("Kas oled nitroseksuaal?", choices=["Jah", "Ei"])
     if nitroseksuaal=="Jah":
         missugunens=buttonbox("Missugune täpsemalt?", choices=["Klassika", "Drift", "Racing"])
+        if missugunens == "Klassika":
+            valikute_nimekiri=klassika
+        elif missugunens == "Drift":
+            valikute_nimekiri=driftikad
+        else:
+            valikute_nimekiri=kiiredpillid
     
     remondivalmis=buttonbox("Kas oled valmis remondiks?", choices=["Jah", "Ei"])
     if remondivalmis=="Ei":
@@ -65,5 +76,6 @@ while jatkub:
 
 
 tekst="Peaksid vaatama järgnevaud autosid: "
+#motlen veel mingid napunaited valja(veic ironic, kui mu enda auto isegi ülevaatuselt läbi ei saanud...)
 soovitused="Meie isiklikud soovitused on.."
 popup=msgbox(tekst)
