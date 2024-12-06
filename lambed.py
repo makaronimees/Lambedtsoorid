d3fb92b9d571041b7b1055f445813944ec04df02
Kriset Lisette Margareth Laiuste <krisetli@ut.ee>
2024-12-06 13:03:19 +0200

    sain lõpuks nii, et xist kinni pannes läheb päriselt kinni

--- lambed.py
+++ lambed.py
@@ -43,12 +43,10 @@ def arvuta_kordaja(remont, kesremondib, nitroeskusaal, budget):
         #sest tak vaja uut autot kui ei taha remontida
         kordaja+=20
 
-
     kordaja+=eelarve[budget]
     kordaja+=ns[nitroeskusaal]
 
     return kordaja
-
 
 def callback():
     msgbox("Aitäh, et usaldasite meid oma otsustusprotsessis!")
@@ -86,58 +84,66 @@ def mida_valjastab(kordaja, ns):
     tekst+=aasta
     return tekst
 
+def x_kontroll(nimi):
+    if nimi == None:
+        quit()
 
-jatkub=True
-while jatkub:
-    koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel", "Kia", "Mazda", "Nissan", "Subaru", "Volvo"]
-    reliable=["Volkswagen", "Škoda","Opel", "Kia", "Ford", "Volvo"]
-    klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
-    driftikad=["BMW", "Nissan", "Mazda", "Opel Omega"]
-    kiiredpillid=["Subaru", "BMW", "Ford", "Audi", "Mercedes-Benz"]
-    tsipalux=["Porsche", "veel midagi"]
 
-    nimi = enterbox("Mis on su nimi?","Tere!","",True)
+koik_margid = ["Audi", "Lada","BMW","Mercedes-Benz","Volkswagen","Ford","Jaguar","Škoda","Opel", "Kia", "Mazda", "Nissan", "Subaru", "Volvo"]
+reliable=["Volkswagen", "Škoda","Opel", "Kia", "Ford", "Volvo"]
+klassika = ["Audi", "BMW", "Mercedes-Benz", "Lada" , "Volkswagen", "Ford",]
+driftikad=["BMW", "Nissan", "Mazda", "Opel Omega"]
+kiiredpillid=["Subaru", "BMW", "Ford", "Audi", "Mercedes-Benz"]
+tsipalux=["Porsche", "veel midagi"]
 
-    kindel_ei = multchoicebox("On sul juba teada, mida kindlasti EI soovi?","Kindlad EI-d",koik_margid,None)
+nimi = enterbox("Mis on su nimi?","Tere!","",True)
+x_kontroll(nimi)
 
-    #tegeleme edaspidi vaid nende autodega, millest kasutaja huvitatud on
-    if kindel_ei != None:
-        for mark in kindel_ei:
-            koik_margid.remove(mark)
-    if len(koik_margid)== 0:
-        break
+kindel_ei = multchoicebox("On sul juba teada, mida kindlasti EI soovi?","Kindlad EI-d",koik_margid,None)
 
-    budgetnupud=["Minimaalne", "Keskmine", "Okei", "Suur", "Lõpmatu"]
-    budget=buttonbox("Mis on su eelarve?", choices=budgetnupud)
-    if budget == "Suur":
-        koik_margid+=tsipalux
-    if budget == "Minimaalne":
-        pass
-    else:
-        nitroseksuaal=buttonbox("Kas oled nitroseksuaal?", choices=["Jah", "Ei"])
-        if nitroseksuaal=="Jah":
-            missugunens=buttonbox("Missugune täpsemalt?", choices=["Klassika", "Drift", "Racing"])
-            if missugunens == "Klassika":
-                valikute_nimekiri=klassika
-            elif missugunens == "Drift":
-                valikute_nimekiri=driftikad
-            else:
-                valikute_nimekiri=kiiredpillid
-    
-    remondivalmis=buttonbox("Kas oled valmis remondiks?", choices=["Jah", "Ei"])
-    if remondivalmis=="Ei":
-        kasise="0"
-        break
-    else:
-        kasise=buttonbox("Kas kavatsed ise teha?", choices=["Jah","Ei"])
-        break
+#tegeleme edaspidi vaid nende autodega, millest kasutaja huvitatud on
+
+try:
+    for mark in kindel_ei:
+        koik_margid.remove(mark)
+except:
+    pass
+
+budgetnupud=["Minimaalne", "Keskmine", "Okei", "Suur", "Lõpmatu"]
+budget=buttonbox("Mis on su eelarve?", choices=budgetnupud)
+if budget == "Suur":
+    koik_margid+=tsipalux
+elif budget == "Minimaalne":
+    headaega = "Kahjuks ei ole võimalik tänapäeval leida minimaalse eelarvega autot, mida on mõtet osta. Kui muudad oma meelt, võid meid uuesti proovida!"
+    msgbox(headaega)
+    quit()
+else:
+    nitroseksuaal=buttonbox("Kas oled nitroseksuaal?", choices=["Jah", "Ei"])
+    x_kontroll(nitroseksuaal)
+    if nitroseksuaal=="Jah":
+        missugunens=buttonbox("Missugune täpsemalt?", choices=["Klassika", "Drift", "Racing"])
+        if missugunens == "Klassika":
+            valikute_nimekiri=klassika
+        elif missugunens == "Drift":
+            valikute_nimekiri=driftikad
+        else:
+            valikute_nimekiri=kiiredpillid
+
+remondivalmis=buttonbox("Kas oled valmis remondiks?", choices=["Jah", "Ei"])
+x_kontroll(remondivalmis)
+if remondivalmis=="Ei":
+    kasise="0"
+
+else:
+    kasise=buttonbox("Kas kavatsed ise teha?", choices=["Jah","Ei"])
+    x_kontroll(kasise)
 
 kordaja=arvuta_kordaja(remondivalmis, kasise, nitroseksuaal, budget)
 valjastatav=mida_valjastab(kordaja, nitroseksuaal)
-    #motlen veel mingid napunaited valja(veic ironic, kui mu enda auto isegi ülevaatuselt läbi ei saanud...)
+#motlen veel mingid napunaited valja(veic ironic, kui mu enda auto isegi ülevaatuselt läbi ei saanud...)
 soovitused="Meie isiklikud soovitused on vältida autosid, mida müüvad teiseringi diilerid. Pigem osta käest kätte või siis otse esindusest. Samuti tasub auto ajalugu vaadata Transpordiameti kodulehelt numbrimärgi taustakontrolli alt ning samuti sealt samast vaadata ka kindlustusjuhtumite ajalugu. Tasuks ka tähelepanu pöörata päritoluriigile, isiklikult ei ostaks kunagi autot, mis on tulnud Lätist, Leedust või Poolast, sest seal ehitatakse 'Frankensteine', ehk mitme auto juppidest kokku pandud autosid. Need autod pole enam nii struktuurselt vastupidavad ning neil võivad olla nähtamatud ning ootamatud vead."
 tekst=nimi+", "+ "peaksid vaatama järgnevaud autosid: " + valjastatav + "\n"+ "\n" + soovitused
 popup=msgbox(tekst)
-    
+
 
 
